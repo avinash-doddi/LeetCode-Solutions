3d161a385f0ae9c4ea15e3406d1d913f0dce677b
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2022-10-24 21:33:27 +0530

    Time: 168 ms (89.45%), Space: 14.9 MB (99.11%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,5 +1,5 @@
 from itertools import combinations
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        arr = [i for i in range(1, n+1)]
-        return map(list,combinations(arr, k))
+        #arr = [i for i in range(1, n+1)]
+        return map(list,combinations([i for i in range(1, n+1)], k))
