e331dc9da5f901db4c7fe6d6868fd4642dafc902
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2022-10-25 21:42:54 +0530

    optimed method

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,3 +1,24 @@
+# O(N LogN) method
+from sortedcontainers import SortedList as slist
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if (k == 1): return nums;
+        finarr = []; d = slist();
+		i = 0; j = 0; n = len(nums); 
+        while(j < n):
+            if (j < k):
+                d.add(nums[j]); j += 1;
+            else:
+                finarr.append(d[-1]);
+                d.remove(nums[i]); d.add(nums[j]); # fixed K size
+                i += 1; j += 1;
+        finarr.append(d[-1]);  #since last iteration is left
+        return finarr 
+
+
+
+
+#**Optimized Code** O(N)
 from collections import deque
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
@@ -11,7 +32,3 @@ class Solution:
             if i >= k-1:
                 result.append(nums[d[0]]);
         return result
-            
-        
-        
-            
