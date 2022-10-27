e07bcebe649528dc51dea8e8949ce01dfe49acec
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2022-10-27 15:53:44 +0530

    Time: 246 ms (32.98%), Space: 18.4 MB (43.47%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,30 +1,9 @@
 from collections import defaultdict as maps
 class Solution:
     def groupAnagrams(self, s: List[str]) -> List[List[str]]:
-        lst = []; n = len(s);
-        d = maps(int); i = 0;
-        while (n > i):
-            if (lst):
-                ins = 0;
-                new = maps(int);
-                for j in s[i]:
-                    new[j] += 1;
-                for sublst in lst:
-                    if (sublst[0] == new):
-                        sublst.append(s[i]); ins = 1;
-                        break;
-                if (ins == 0):
-                    lst.append([new, s[i]]);
-                del new;
-                    
-            else:
-                del d; d = maps(int);
-                for k in s[i]:
-                    d[k] += 1;
-                lst.append([d, s[i]]);
-            i += 1;
-        for sublst in lst:
-            del sublst[0]
-        return lst
+        reference = maps(list)
+        for word in s:
+            reference[tuple(sorted(word))].append(word)
+        return reference.values()
                 
         
