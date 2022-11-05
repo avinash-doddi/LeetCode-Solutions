3e62aaef7a3adcc0f3c94e135c7939afcaa3b900
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2022-11-05 18:46:45 +0530

    Time: 715 ms (8.76%), Space: 21.8 MB (5.06%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,31 @@
+class Solution:
+    def traverse(self, strr, s, n, r, i, j):
+        self.i = i; self.j = j;
+        if (i > n or i <= 0 or self.k >= n): return
+        strr[i][j] = s[self.k]; self.k += 1;
+        self.traverse(strr, s, n, r, i-1, j+1);
+        
+    
+    def convert(self, s: str, r: int) -> str:
+        if (r == 1): return s
+        n = len(s);
+        strr = [[""]*1000 for i in range(r)];
+        self.k = 0; self.i = 0; self.j = 0;
+        while(self.k < n):
+            if (self.i == r-1):
+                #self .i -= 1; self.j += 1;
+                self.traverse(strr, s, n, r, self.i, self.j);
+            else:
+                strr[self.i][self.j] = s[self.k];
+                self.i += 1; self.k += 1;
+        finalstr = ''
+        for lst in strr:
+            finalstr += ("".join(lst));
+        return finalstr
+            
+            
+            
+                
+                
+                
+        
