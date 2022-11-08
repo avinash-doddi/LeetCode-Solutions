73474269cbba610a6c56072257c817835670b86d
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2022-11-08 16:32:36 +0530

    Time: 69 ms (51.07%), Space: 13.8 MB (61.93%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -0,0 +1,11 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = []; n = len(s)
+        for i in range(n):
+            if (stack):
+                if abs(ord(stack[-1]) - ord(s[i])) == 32:
+                    stack.pop();
+                else: stack.append(s[i]);
+            else:
+                stack.append(s[i]);
+        return "".join(stack)
