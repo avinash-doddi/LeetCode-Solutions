0e95ad4c73efc6fc2480f06e7d1c0959146b6b3c
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2022-12-06 14:34:29 +0530

    Time: 38 ms (84.87%), Space: 13.9 MB (79.23%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,17 +1,17 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        def bfs(init, final, temp, target, k, i, n):
+        def dfs(init, final, temp, target, k, i, n):
             if (k == 0):
                 if (target == 0): 
                     final.append(temp[:]); return
                 else: return
             if (i == n or target < 0): return
             temp.append(init[i]);
-            bfs(init, final, temp, target-init[i], k-1, i+1, n)
+            dfs(init, final, temp, target-init[i], k-1, i+1, n)
             temp.pop()
-            bfs(init, final, temp, target, k, i+1, n)
+            dfs(init, final, temp, target, k, i+1, n)
         
         init = [i for i in range(1,10)]
         final = []; temp = []
-        bfs(init, final, temp, n, k, 0, 9)
+        dfs(init, final, temp, n, k, 0, 9)
         return final
