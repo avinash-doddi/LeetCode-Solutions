1ec86361f09a34393d6041c1911a8db2bb7e49e4
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2022-12-06 12:25:59 +0530

    Time: 69 ms (88.30%), Space: 13.9 MB (94.25%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,16 +1,13 @@
 class Solution:
-    
-    def recur(self, candidates, i, n, target):
-        if (target == 0):
-            self.arr.append(self.ref[:]); 
-            return;
-        if (i >= n or target < 0): return;
-        self.ref.append(candidates[i]);
-        self.recur(candidates, i, n, target-candidates[i]);
-        self.ref.pop();
-        self.recur(candidates, i+1, n, target);
-        
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        self.arr = []; self.ref = []; n = len(candidates);
-        self.recur(candidates, 0, n, target);
-        return self.arr
+    def combinationSum(self, c: List[int], target: int) -> List[List[int]]:
+        def bfs(final, c, target, i, n, temp):
+            if (i == n or target < 0): return
+            if (target == 0):
+                final.append(temp[:]); return
+            temp.append(c[i]);
+            bfs(final, c, target - c[i], i, n, temp)
+            temp.pop()
+            bfs(final, c, target, i+1, n, temp)
+        n = len(c); temp = []; final = []
+        bfs(final, c, target, 0, n, temp)
+        return final
