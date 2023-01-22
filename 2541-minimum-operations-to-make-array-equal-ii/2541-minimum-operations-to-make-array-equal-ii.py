ccc4b5d0506092d04f9c825e849171c65cb00879
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2023-01-22 23:06:21 +0530

    Time: 1091 ms (75.00%), Space: 30.4 MB (8.33%) - LeetHub

--- 2541-minimum-operations-to-make-array-equal-ii/2541-minimum-operations-to-make-array-equal-ii.py
+++ 2541-minimum-operations-to-make-array-equal-ii/2541-minimum-operations-to-make-array-equal-ii.py
@@ -1,28 +1,19 @@
 class Solution:
     def minOperations(self, a: List[int], b: List[int], k: int) -> int:
-        high = []; low = []; n = len(a); h,l = 0,0; total = 0;
+        high = 0; low = 0; n = len(a);total = 0;
         for i in range(n):
             if k != 0:
                 if abs(a[i] - b[i]) % k != 0: return -1;
                 if a[i] != b[i]: 
                     total += (abs(a[i] - b[i]))//k;
                     if a[i] > b[i]:
-                        high.append(a[i] - b[i]); h += 1;
+                        high += (a[i] - b[i]); 
                     if a[i] < b[i]:
-                        low.append(b[i] - a[i]); l += 1;
+                        low += (b[i] - a[i]);
             else:
                 if a[i] != b[i]: return -1
-        if total & 1 or h == n or l == n: return -1
-        i = 0; j = 0; high.sort(); low.sort();
-        while(i < h and j < l):
-            if high[i] > low[j]:
-                high[i] -= low[j]; low[j] = 0; j += 1;
-            else:
-                low[j] -= high[i]; high[i] = 0; i += 1;
-        for i in high: 
-            if i: return -1
-        for j in low: 
-            if j: return -1
+        if total & 1: return -1
+        if abs(low - high) != 0: return -1
         return total//2
         
         
