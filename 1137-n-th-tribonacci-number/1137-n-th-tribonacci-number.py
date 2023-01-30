2bd55ad96b6b4630ff207393c1681894948af2b5
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2023-01-30 23:41:58 +0530

    Time: 37 ms (44.79%), Space: 13.8 MB (95.87%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,10 +1,6 @@
 class Solution(object):
     def tribonacci(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        
+       
         if n==0: return 0
         elif n==1: return 1
         elif n==2: return 1
