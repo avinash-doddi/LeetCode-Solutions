62a08ce6aea9fae8ded77136bf7d288814df7001
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2023-02-23 10:16:59 +0530

    Time: 716 ms (22.03%), Space: 15 MB (7.32%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -1,6 +1,13 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         n = len(nums); one = 0;
+        """
+            the code in try part is similar to sliding window implementation
+            we take start, end variables. end = i for every iteration.
+            we count the zeroes and we insert the index of that 0 in dict.
+            then, when count == k, then we remove the first element of list in dict.
+            for every iteration, we calculate max i.e maxx = max(maxx, end - start+1);
+        """
         try:
             d = {0:[]}; zro = 0; start = -1;
             maxx, end = -1,0; temp = 0;
@@ -16,7 +23,7 @@ class Solution:
                         d[0].append(i); zro += 1;
                 maxx = max(maxx, end - start+1);
             return max(maxx, end - start+1)
-        except Exception:
+        except Exception: # for k = 0 case ;P we might get Runtime Error;
             maxx = 0;
             for i in range(n):
                 if nums[i] == 1:
