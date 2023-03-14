aaa6c6f7216d950046baa832e384050428de8aac
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2023-03-14 21:15:18 +0530

    Time: 29 ms (84.93%), Space: 13.8 MB (54.57%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -19,5 +19,17 @@ class Solution:
         arr = []; summ = 0; self.sm = []
         traverse(root, arr, summ)
         return sum(self.sm)
+"""
+C++ EXCEL SOLUTN
+class Solution {
+public:
+    int sumNumbers(TreeNode* root,int sum=0) {
+        if(!root) return 0;
+        sum = sum*10 + root->val;
+        if(!root->left && !root->right)
+            return sum;
+        return sumNumbers(root->left,sum)+sumNumbers(root->right,sum);
+    }
+};"""
             
             
