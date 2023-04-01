4c5ef4df5660cea08f2ef962c37f930b0077c8b7
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2023-04-01 15:54:01 +0530

    Time: 260 ms (13.58%), Space: 15.5 MB (58.56%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,13 +1,19 @@
 class Solution:
-    def search(self, a: List[int], k: int) -> int:
-        s = 0; e = len(a) - 1; mid = (s + e)//2;
-        if (a[mid] == k): return mid;
-        while(s < e):
-            mid = (s+e)//2;
-            if (a[mid] == k): return mid;
-            if (a[mid] < k): s = mid+1;
-            if (a[mid] > k): e = mid-1;
-        if (a[s] == k): return s;
-        if (a[e] == k): return e;
-        else: return -1
+    def search(self, a: List[int], x: int) -> int:
+        n = len(a); low, high = 0, n-1;
+        while low <= high and x >= a[low] and x <= a[high]:
+            if low == high:
+                if a[low] == x:
+                    return low
+                return -1
+            
+            pos = low + int(((high-low)/(a[high]-a[low])) * (x - a[low]));
+            
+            if x == a[pos]: return pos
+            if x < a[pos]:
+                high = pos - 1;
+            else: 
+                low = pos + 1
+        return -1
+            
                 
