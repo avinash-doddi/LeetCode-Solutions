afdce5b231ce2d27d9348a1a09c69970409da862
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2023-10-13 16:38:18 +0530

    Time: 46 ms (10.52%), Space: 17.1 MB (58.99%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.py
@@ -0,0 +1,72 @@
+# """
+# This is MountainArray's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class MountainArray:
+#    def get(self, index: int) -> int:
+#    def length(self) -> int:
+
+class Solution:
+    def findInMountainArray(self, target: int, m: 'MountainArray') -> int:
+        #find peak::
+        n = m.length(); left = 0; right = n-1;
+        mid = 0; peak = -1; peak_index = -1; calls = 0
+        
+        while left <= right:
+            # mid = (left + right)//2;
+            mid = left + (right - left)//2
+            if mid < 0 or mid > n-1: break
+            curr = m.get(mid);
+            before, after = m.get(mid-1), m.get(mid+1)
+            calls += 3;
+            if before < curr > after:
+                peak = curr; peak_index = mid; break;
+            # else::
+            if curr < after:
+                left = mid; 
+            elif curr < before:
+                right = mid;
+                
+        if peak == target: return peak_index
+        print("at peak calls = ", calls)
+        
+        # check leftside of array::
+        left = 0; right = peak_index;
+        while left <= right:
+            mid = left + (right - left)//2
+            if mid < 0 or mid > peak_index: break
+            curr = m.get(mid); calls += 1;
+            if curr == target: 
+                print("left calls = ", calls)
+                return mid;
+            #else::
+            if curr < target:
+                left = mid+1;
+            else:
+                right = mid-1;
+            
+        # check rightside of array::
+        print(peak_index)
+        left = peak_index; right = n-1;
+        while left <= right:
+            mid = left + (right - left)//2;
+            if mid < peak_index or mid > n-1: break
+            curr = m.get(mid); calls += 1;
+            if curr == target:
+                print("right calls = ", calls)
+                return mid;
+            #else::
+            if curr > target:
+                left = mid+1;
+            else:
+                right = mid-1;
+            
+        print("overall calls = ", calls)
+        
+        #if element not present::
+        return -1
+            
+            
+        
+        
+        
