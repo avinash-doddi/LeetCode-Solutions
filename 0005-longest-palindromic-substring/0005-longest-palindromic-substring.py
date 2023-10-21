f6cbc8ddc82eb93e33b6c5c620e8ebf93a0d9151
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2023-10-21 12:45:15 +0530

    Time: 327 ms (94.32%), Space: 16.2 MB (96.12%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,13 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s); r = 0; m = 0; fin = ''
-        if s == s[::-1]: return s
+        def check_left_right(l,r):
+            while l >= 0 and r < n and s[l] == s[r]:
+                l -= 1; r += 1;
+            return s[l+1:r]
+        """
+        we take a particular index and check if left and right of it are equal
+        and we go on like that till they are not equal.
+        complexity: O(n^2)
+        """
+        n = len(s)
+        res = ""
         for i in range(n):
-            ref = ''; r = 0;
-            for j in range(i, n):
-                ref += s[j]; r += 1;
-                if (ref == ref[::-1]):
-                    if (r > m):
-                        m = r; fin = ref;
-        return fin
+            sub1 = check_left_right(i,i)
+            if len(sub1) > len(res):
+                res = sub1;
+            sub1 = check_left_right(i,i+1) # for cases like "cbbd" => "bb"
+            if len(sub1) > len(res):
+                res = sub1;
+        return res
                 
+        
