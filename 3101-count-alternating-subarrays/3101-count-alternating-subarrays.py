4798b5ff94172e7b79ea406ae5ab22dcf2f74d7b
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2024-03-31 14:50:21 +0530

    Time: 804 ms (33.33%), Space: 20.1 MB (66.67%) - LeetHub

--- 3101-count-alternating-subarrays/3101-count-alternating-subarrays.py
+++ 3101-count-alternating-subarrays/3101-count-alternating-subarrays.py
@@ -1,19 +1,15 @@
 class Solution:
     def countAlternatingSubarrays(self, nums: List[int]) -> int:
         if len(nums) == 1: return 1
-        subarr, cnt = [], 0
+        cnt, total = 0, 0
         for i in range(len(nums)-1):
             if nums[i] != nums[i+1]:
                 cnt += 1;
             else:
                 if cnt: 
-                    subarr.append(cnt+1); cnt = 0
-                else: subarr.append(1)
-        if cnt: subarr.append(cnt + 1)
-        else: subarr.append(1);
-        total = 0
-        #print(subarr)
-        for i in subarr:
-            total += ((i*(i+1))//2)
+                    total += ((cnt+1) * (cnt+2))//2;  cnt = 0
+                else: total += 1;
+        if cnt: total += ((cnt+1) * (cnt+2))//2
+        else: total += 1
         return total
         
